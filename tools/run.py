8f811ba8ef5874b35a8422831700bc0a00a771b8
Christian Maillefaud <christian@lojaintegrada.com.br>
2016-10-31 19:03:43 -0200

    Atualiza versão

--- tools/run.py
+++ tools/run.py
@@ -24,7 +24,7 @@ from tools.utils import confirma
 from tools.deploy import run_deploy
 from tools.docker import run_debug, run_telnet, run_test, run_bash
 
-VERSION = "2.2.1"
+VERSION = "2.2.2"
 
 
 def main():
