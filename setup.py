082ec0e0308e7093c028b54d86b19f02927d284a
Chris Maillefaud <chrismaillefaud@gmail.com>
2019-07-30 21:23:19 -0300

    Remove Python 3.5 support

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
